7a844b926ed079dc0bc6ccb0385fc3b36b6abddc
Chris Shepherd <christopher.shepherd@imaginecurve.com>
2020-09-21 22:59:54 +0100

    return api gateway response from get-live-matches

--- live-match/internal/handler/aws/handler.go
+++ live-match/internal/handler/aws/handler.go
@@ -2,8 +2,11 @@ package aws
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"net/http"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/cshep4/lambda-go/log"
 
 	"github.com/cshep4/premier-predictor-functions/common/domain"
@@ -31,13 +34,30 @@ func (h *Handler) CheckLiveMatches(ctx context.Context) error {
 	return nil
 }
 
-func (h *Handler) GetLiveMatches(ctx context.Context) ([]domain.LiveMatch, error) {
+func (h *Handler) GetLiveMatches(ctx context.Context) (events.APIGatewayProxyResponse, error) {
 	liveMatches, err := h.Service.RetrieveLiveMatches(ctx)
 	if err != nil {
 		log.Error(ctx, "err_getting_live_matches", log.ErrorParam(err))
 
-		return nil, fmt.Errorf("retrieve_live_matches: %w", err)
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+			Headers:    map[string]string{"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"},
+		}, fmt.Errorf("retrieve_live_matches: %w", err)
 	}
 
-	return liveMatches, nil
+	body, err := json.Marshal(liveMatches)
+	if err != nil {
+		log.Error(ctx, "err_json_marshal", log.ErrorParam(err))
+
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+			Headers:    map[string]string{"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"},
+		}, fmt.Errorf("json_marshal: %w", err)
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Headers:    map[string]string{"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"},
+		Body:       string(body),
+	}, nil
 }

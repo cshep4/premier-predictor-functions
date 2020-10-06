5a433759256e41449c4bd926b1d1798e4995bb18
Chris Shepherd <christopher.shepherd@imaginecurve.com>
2020-10-06 18:51:14 +0100

    fix invocation failures

--- live-match/cmd/get-live-matches/main.go
+++ live-match/cmd/get-live-matches/main.go
@@ -5,11 +5,8 @@ import (
 	"fmt"
 
 	"github.com/aws/aws-sdk-go/service/sfn"
-	"github.com/cshep4/lambda-go/heartbeat/middleware"
-	"github.com/cshep4/lambda-go/heartbeat/mongo"
 	"github.com/cshep4/lambda-go/lambda"
 	"github.com/cshep4/lambda-go/log"
-	"github.com/cshep4/lambda-go/mongodb"
 	"github.com/cshep4/premier-predictor-functions/common/api"
 	"github.com/cshep4/premier-predictor-functions/common/redis"
 
@@ -51,29 +48,10 @@ func setup(ctx context.Context) error {
 		return fmt.Errorf("new_match_updater: %w", err)
 	}
 
-	service, err := live.New(api.InjectApiRequester(), redis.InjectRedisRepository(), updater)
+	handler.Service, err = live.New(api.InjectApiRequester(), redis.InjectRedisRepository(), updater)
 	if err != nil {
 		return fmt.Errorf("new_live_match_service: %w", err)
 	}
-
-	mongoClient, err := mongodb.New(ctx)
-	if err != nil {
-		return fmt.Errorf("new_mongo_client: %w", err)
-	}
-
-	heartBeater, err := mongo.New(ctx, functionName, mongoClient)
-	if err != nil {
-		return fmt.Errorf("new_heartbeater: %w", err)
-	}
-
-	middleware, err := heartbeat.New(heartBeater)
-	if err != nil {
-		return fmt.Errorf("new_heartbeat_middleware: %w", err)
-	}
-
-	runner.Apply(lambda.WithPreExecute(middleware.PreExecute))
-
-	handler.Service = service
 
 	return nil
 }
--- live-match/cmd/live-match-check/main.go
+++ live-match/cmd/live-match-check/main.go
@@ -9,11 +9,8 @@ import (
 	awsconfig "github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sfn"
-	"github.com/cshep4/lambda-go/heartbeat/middleware"
-	"github.com/cshep4/lambda-go/heartbeat/mongo"
 	"github.com/cshep4/lambda-go/lambda"
 	"github.com/cshep4/lambda-go/log"
-	"github.com/cshep4/lambda-go/mongodb"
 	"github.com/cshep4/premier-predictor-functions/common/api"
 	"github.com/cshep4/premier-predictor-functions/common/redis"
 
@@ -72,29 +69,10 @@ func setup(ctx context.Context) error {
 		return fmt.Errorf("new_match_updater: %w", err)
 	}
 
-	service, err := live.New(api.InjectApiRequester(), redis.InjectRedisRepository(), updater)
+	handler.Service, err = live.New(api.InjectApiRequester(), redis.InjectRedisRepository(), updater)
 	if err != nil {
 		return fmt.Errorf("new_live_match_service: %w", err)
 	}
-
-	mongoClient, err := mongodb.New(ctx)
-	if err != nil {
-		return fmt.Errorf("new_mongo_client: %w", err)
-	}
-
-	heartBeater, err := mongo.New(ctx, functionName, mongoClient)
-	if err != nil {
-		return fmt.Errorf("new_heartbeater: %w", err)
-	}
-
-	middleware, err := heartbeat.New(heartBeater)
-	if err != nil {
-		return fmt.Errorf("new_heartbeat_middleware: %w", err)
-	}
-
-	runner.Apply(lambda.WithPreExecute(middleware.PreExecute))
-
-	handler.Service = service
 
 	return nil
 }
